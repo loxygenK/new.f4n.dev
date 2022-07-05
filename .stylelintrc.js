860a82c6cc71129db778a3a22140c626091488d4
loxygenK <me@loxygen.dev>
2022-07-05 12:30:20 +0900

    🔧 Loosen configurations for Stylelint

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -4,4 +4,15 @@ module.exports = {
     "stylelint-config-standard-scss",
     "stylelint-config-prettier"
   ],
+  rules: {
+    // Selector should use snake_case
+    // (To avoid problems around CSS Modules)
+    "selector-class-pattern": "[a-zA-Z][0-9a-zA-Z_]*",
+
+    // Allow any empty lient between CSS Declaration
+    "declaration-empty-line-before": null,
+
+    // Always use percentage specification for alpha value (opacity)
+    "alpha-value-notation": "percentage"
+  }
 }
