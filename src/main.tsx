f13c2b4c5cc5478585c150b84689dcd7b3c8f800
loxygenk <me@loxygen.dev>
2023-01-02 15:40:47 +0900

    🎨 Initialize the style using `ress` and additional my own reset css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,10 @@ import { render } from "solid-js/web";
 import { Router, useRoutes } from "@solidjs/router";
 import routes from "~solid-pages";
 
+import "ress";
+
+import "~/styles/base.scss";
+
 render(() => {
   const Routes = useRoutes(routes);
   return (
